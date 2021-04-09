1ecb327b82751457eaee45906c33c1990aff9cc9
ahmed7752 <61713723+Ahmed7752@users.noreply.github.com>
2021-04-09 17:41:31 +0100

    added commands to readme

--- README.txt
+++ README.txt
@@ -15,3 +15,14 @@ ssh -i C:\Users\Ahmed\Documents\computer-science-repo\Third-Year\CloudComputting
 Copy fiels to EC2 command:
 
 scp -i <path to private key pair> -r <path to host file> ec2-user@52.202.252.241:/var/www/html
+
+
+<<Ignore>>
+D:\computer-science-repo\Third-Year\Cloud-Computing\CloudComputtingWebAppGroup29\WebServerpemkeypair.pem
+D:\computer-science-repo\Third-Year\Cloud-Computing\CloudComputtingWebAppGroup29\xampp2\htdocs\Application\*
+<<>>
+
+Connect to database command:
+
+mysql -h websitedatabase.cfv3cdy44ueb.us-east-1.rds.amazonaws.com -P 3306 -u admin -p
+
--- xampp2/htdocs/Application/connection.php
+++ xampp2/htdocs/Application/connection.php
@@ -1,9 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "18124225";// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
+  $hostname = "db-for-website.cfv3cdy44ueb.us-east-1.rds.amazonaws.com";
+  $username = "admin";
+  $password = "CloudComputing";
+  $dbname = "18124225";
+  $port = "3306"
+// Create connection
+
+  $conn = new mysqli($hostname,$username, $password, $dbname, $port);
+  if ($conn->connect_error) {
+    die("Connection failed because: " . $conn->connect_error);
+  }
+
+?>
